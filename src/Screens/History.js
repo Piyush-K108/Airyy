261c17821620071b69076d5b8927c2008f28d2ef
Piyush-K108 <gwar619908@gmail.com>
2024-01-17 15:31:12 +0530

    DOne

--- src/Screens/History.js
+++ src/Screens/History.js
@@ -1,8 +1,13 @@
-import React from 'react';
-
+import { View, Text } from 'react-native'
+import React from 'react'
 
 const History = () => {
-  return <View></View>;
-};
+  return (
 
-export default History;
+        <View className='h-screen w-screen justify-center items-center'>
+        <Text className='text-black '>History</Text>
+      </View>
+  )
+}
+
+export default History
